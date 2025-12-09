1a80ce9cefe32d9af143b580fd06af23ea9211dc
freiremat <urbsfreire@gmail.com>
2025-12-09 16:21:11 -0300

    tasks: - create home comp - set reactive forms on html - set validators on TS - set logic to login/signup form

---
+++ src/app/modules/home/home.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  loginCard = true;
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  signupForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+  }
+
+  onSubmitLoginForm(): void {
+    console.log('teste', this.loginForm.value);
+  }
+
+  onSubmitSignUpForm(): void {
+    console.log('teste', this.signupForm.value);
+  }
+}
