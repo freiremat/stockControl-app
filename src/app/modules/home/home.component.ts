f2ad87638a2381eafd5a3fc6820b07dd3194db12
freiremat <urbsfreire@gmail.com>
2026-01-07 15:53:52 -0300

    tasks: - create logic of signupForm and LoginForm

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
+import { AuthRequest } from 'src/app/models/interfaces/user/AuthRequest';
+import { SignUpUserRequest } from 'src/app/models/interfaces/user/SignUpUserRequest';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-home',
@@ -20,16 +24,41 @@ export class HomeComponent implements OnInit {
     password: ['', Validators.required]
   })
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private userService: UserService, private cookieService: CookieService) { }
 
   ngOnInit() {
   }
 
   onSubmitLoginForm(): void {
-    console.log('teste', this.loginForm.value);
+    if (this.loginForm.value && this.loginForm.valid) {
+      this.userService.authUser(this.loginForm.value as AuthRequest)
+        .subscribe({
+          next: (response) => {
+            if (response) {
+              this.cookieService.set('USER_INFO', response?.token)
+
+              this.loginForm.reset();
+            }
+          },
+          error: (err) => console.log(err),
+        })
+    }
   }
 
   onSubmitSignUpForm(): void {
-    console.log('teste', this.signupForm.value);
+    if (this.signupForm.value && this.signupForm.valid) {
+      this.userService
+        .signupUser(this.signupForm.value as SignUpUserRequest)
+        .subscribe({
+          next: (response) => {
+            if (response) {
+              alert('usuario criado com sucesso');
+              this.signupForm.reset();
+              this.loginCard = true;
+            }
+          },
+          error: (err) => console.log(err),
+        });
+    }
   }
 }
