814e83e60981d1f4108d1964df9aa61d857632ca
freiremat <urbsfreire@gmail.com>
2026-02-04 12:00:51 -0300

    tasks: - setou route for products page - add lazy loading on the route - implement search for products

--- src/app/modules/products/page/products-home/products-home.component.ts
+++ src/app/modules/products/page/products-home/products-home.component.ts
@@ -1,14 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { Subject, takeUntil } from 'rxjs';
+import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
+import { ProductsService } from 'src/app/services/products/products.service';
+import { ProductsDataTransferService } from 'src/app/shared/services/products/products-data-transfer.service';
 
 @Component({
   selector: 'app-products-home',
   templateUrl: './products-home.component.html',
 })
-export class ProductsHomeComponent implements OnInit {
+export class ProductsHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+  public productsList: Array<GetAllProductsResponse> = [];
 
-  constructor() { }
+  constructor(private productsService: ProductsService, private productsDataTransferService: ProductsDataTransferService, private router: Router, private messaegeService: MessageService) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getServiceProductsData();
+  }
+
+  getServiceProductsData() {
+    const productsLoaded = this.productsDataTransferService.getProductsData();
+
+    if (productsLoaded.length > 0) {
+      this.productsList = productsLoaded;
+    } else {
+      this.getAPIProductsDatas();
+    }
+
+  }
+
+  getAPIProductsDatas() {
+    this.productsService.getAllProducts()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          this.productsList = response;
+        },
+        error: (error) => {
+          console.error('Error fetching products:', error);
+          this.messaegeService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Falha ao carregar produtos. Redirecionando para tela de dashboard.',
+            life: 2500,
+          })
+          this.router.navigate(['/dashboard']);
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
 }
