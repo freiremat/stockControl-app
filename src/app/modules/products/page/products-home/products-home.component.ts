30351f5de522a35301237bc976cfeac949435eec
freiremat <urbsfreire@gmail.com>
2026-02-04 11:28:11 -0300

    tasks: - generate products module - start config route

---
+++ src/app/modules/products/page/products-home/products-home.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-home',
+  templateUrl: './products-home.component.html',
+})
+export class ProductsHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
