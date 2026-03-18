660f52ee4f67bd019421b100660f69d013b4ad23
freiremat <urbsfreire@gmail.com>
2026-03-18 16:27:18 -0300

    tasks: - implement categories table   - create comp categories-home and set method to fetch all categories   - create comp categories-table   - add routing to categories comp   - create categories module and add imports   - create categories routing comp

---
+++ src/app/modules/categories/categories.module.ts
@@ -0,0 +1,53 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CATEGORIES_ROUTES } from './categories.routing';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
+
+//primeng
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { DialogService, DynamicDialogModule } from 'primeng/dynamicdialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { TooltipModule } from 'primeng/tooltip';
+import { ConfirmationService } from 'primeng/api';
+import { CategoriesTableComponent } from './components/categories-table/categories-table.component';
+import { CategoriesHomeComponent } from './page/categories-home/categories-home.component';
+import { RippleModule } from "primeng/ripple";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(CATEGORIES_ROUTES),
+    SharedModule,
+    HttpClientModule,
+    //primeng
+    CardModule,
+    ButtonModule,
+    TableModule,
+    InputMaskModule,
+    InputSwitchModule,
+    InputTextModule,
+    InputTextareaModule,
+    InputNumberModule,
+    DynamicDialogModule,
+    DropdownModule,
+    ConfirmDialogModule,
+    TooltipModule,
+    RippleModule
+],
+  declarations: [CategoriesHomeComponent, CategoriesTableComponent],
+  providers: [DialogService, ConfirmationService]
+})
+export class CategoriesModule { }
---
+++ src/app/modules/categories/page/categories-home/categories-home.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { DialogService } from 'primeng/dynamicdialog';
+import { Subject, take, takeUntil } from 'rxjs';
+import { GetCategoriesResponse } from 'src/app/models/interfaces/categories/responses/GetCategoriesResponse';
+import { CategoriesService } from 'src/app/services/categories/categories.service';
+
+@Component({
+  selector: 'app-categories-home',
+  templateUrl: './categories-home.component.html',
+})
+export class CategoriesHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+  public categoriesDatas: Array<GetCategoriesResponse> = [];
+
+  constructor(
+    private categoriesService: CategoriesService,
+    private dialogService: DialogService,
+    private messageService: MessageService,
+    private confirmationService: ConfirmationService,
+    private router: Router) { }
+
+  ngOnInit() {
+    this.getAllCategories()
+  }
+
+  getAllCategories() {
+    this.categoriesService.getAllCategories().pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if (response.length > 0) {
+            this.categoriesDatas = response;
+          }
+        },
+        error: (err) => {
+          console.log(err);
+          this.messageService.add(
+            {
+              severity: 'error',
+              summary: 'Error',
+              detail: 'Erro ao buscar categorias.'
+            });
+          this.router.navigate(['/dashboard'])
+        },
+      })
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+}
